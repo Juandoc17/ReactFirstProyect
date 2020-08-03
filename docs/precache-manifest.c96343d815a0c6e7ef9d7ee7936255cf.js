066dcb5bfe2b6a7296bf6f1076f58d4901633232
Juan Ocampo <juan.doc17@hotmail.com>
2020-08-03 14:48:29 -0500

    Docs agregado

---
+++ docs/precache-manifest.c96343d815a0c6e7ef9d7ee7936255cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49cb54e572ed1bcaf26a0674e5c1b2c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fe9638a8b2518e19fbf",
+    "url": "/static/css/main.896e0d26.chunk.css"
+  },
+  {
+    "revision": "26227e71ad7d4f1513b6",
+    "url": "/static/js/2.d7cf9fae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7cf9fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe9638a8b2518e19fbf",
+    "url": "/static/js/main.ecc2d0e5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
